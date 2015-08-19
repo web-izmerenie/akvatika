e767a5aec74cd6827a800489afcf6ecb8470781a
Andrey Chechkin <andrey.chechkin@web-izmerenie.ru>
2015-08-19 13:04:05 +0300

    Добавлена JS функция по выбору способа доставки

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,24 @@
 $(function(){
 
+	//init plugins
+	$('.cert ul').slick({
+		variableWidth: true,
+		dots: false,
+		slidesToShow: 4,
+  	slidesToScroll: 4,
+	});
+	$('.cert .slick-slide a').fancybox({
+		centerOnScroll: true,
+		padding: 0,
+		titleShow: false,
+		helpers: {
+			overlay: {
+				locked: false
+			}
+		}
+	});
+	$('input[type="radio"]').styler();
+
 	function ankorAnimate(target){
 		$(target).click(function(event) {
 			event.preventDefault();
@@ -53,27 +72,50 @@ function loadContent(clickLoad, container) {
 		});
 	}
 
+	function order(){
+		var order = $('#form-order');
+		var checkboxes = $('#checkbox .jq-radio');
+		var formObj = order.find('#adress-form');
+		var areaObj = order.find('#area-text');
+		var pickupObj = order.find('#pickup-text');
+		var checkedObj = order.find('.checked');
+
+		function switchRadio(elem){
+			switch (elem) {
+				case 'rostov-styler':
+					$(formObj).fadeIn();
+					break;
+
+				case 'aksai-styler':
+					$(formObj).fadeIn();
+					break;
+
+				case 'area-styler':
+					$(areaObj).fadeIn();
+					break;
+
+				case 'pickup-styler':
+					$(pickupObj).fadeIn();
+					break;
+			}
+		}
+
+		var idObj = $(checkedObj).attr('id');
+
+		switchRadio(idObj);
+
+		$(checkboxes).on('click', function(){
+			var idObj = $(this).attr('id');
+
+			$('.blocks').hide();
+
+			switchRadio(idObj);
+		});
+	}
+
+	order();
 	ankorAnimate('.ankor');
 	loadContent('#menu li a', '#menu-window');
 	loadContent('.advents li a', '#advents-window');
 	closeWindow('.overlay', '.module-window');
-
-	//init plugins
-	$('input').styler();
-	$('.cert ul').slick({
-		variableWidth: true,
-		dots: false,
-		slidesToShow: 4,
-  	slidesToScroll: 4,
-	});
-	$('.cert .slick-slide a').fancybox({
-		centerOnScroll: true,
-		padding: 0,
-		titleShow: false,
-		helpers: {
-			overlay: {
-				locked: false
-			}
-		}
-	});
 });
