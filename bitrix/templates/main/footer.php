a23fb4dc362da0feebfe53ea61921cd2e2dbbf6e
Andrey Chechkin <andrey.chechkin@web-izmerenie.ru>
2015-09-03 15:47:18 +0300

    Add yandex counter

--- bitrix/templates/main/footer.php
+++ bitrix/templates/main/footer.php
@@ -11,6 +11,37 @@ if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
 			<section id="alert-window" class="module-window">
 				<div class="wrap"><a href="#" class="close"></a></div>
 			</section>
+			<!-- Yandex.Metrika counter -->
+			<script type="text/javascript">
+			(function (d, w, c) {
+			(w[c] = w[c] || []).push(function() {
+			try {
+			w.yaCounter32297939 = new Ya.Metrika({
+			id:32297939,
+			clickmap:true,
+			trackLinks:true,
+			accurateTrackBounce:true,
+			webvisor:true
+			});
+			} catch(e) { }
+			});
+
+			var n = d.getElementsByTagName("script")[0],
+			s = d.createElement("script"),
+			f = function () { n.parentNode.insertBefore(s, n); };
+			s.type = "text/javascript";
+			s.async = true;
+			s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+			if (w.opera == "[object Opera]") {
+			d.addEventListener("DOMContentLoaded", f, false);
+			} else { f(); }
+			})(document, window, "yandex_metrika_callbacks");
+			</script>
+			<noscript><div><img
+			src="https://mc.yandex.ru/watch/32297939" style="position:absolute;
+			left:-9999px;" alt="" /></div></noscript>
+			<!-- /Yandex.Metrika counter -->
 		</div>
 	</body>
 </html>
